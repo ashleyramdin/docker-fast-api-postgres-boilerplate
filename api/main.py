c4b993b4de9494f5862ac532acc747b9f7b71c61
ashleyramdin <ashleyramdin@outlook.com>
2023-10-05 17:03:53 -0400

    Uncommented routes

--- api/main.py
+++ api/main.py
@@ -14,22 +14,22 @@ async def root():
     return RedirectResponse(app.docs_url)
 
 
-# @app.post("/create/{key}", status_code=HTTP_201_CREATED)
-# def create(key: str, value: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
-#     with todo_repository as repo:
-#         repo.save(Todo(key=key, value=value))
+@app.post("/create/{key}", status_code=HTTP_201_CREATED)
+def create(key: str, value: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
+    with todo_repository as repo:
+        repo.save(Todo(key=key, value=value))
 
 
-# @app.get("/get/{key}", response_model=Optional[Todo])
-# def get(key: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
-#     with todo_repository as repo:
-#         return repo.get_by_key(key)
+@app.get("/get/{key}", response_model=Optional[Todo])
+def get(key: str, todo_repository: TodoRepository = Depends(create_todo_repository)):
+    with todo_repository as repo:
+        return repo.get_by_key(key)
 
 
-# @app.get("/find", response_model=List[Todo])
-# def find(todo_filter: TodoFilter = Depends(), todo_repository: TodoRepository = Depends(create_todo_repository)):
-#     with todo_repository as repo:
-#         return repo.get(todo_filter)
+@app.get("/find", response_model=List[Todo])
+def find(todo_filter: TodoFilter = Depends(), todo_repository: TodoRepository = Depends(create_todo_repository)):
+    with todo_repository as repo:
+        return repo.get(todo_filter)
 
 @app.get("/home")
 def home():
